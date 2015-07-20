acf2f038cde278ce311a2150e8c697e0672b9ba6
zhangxin5 <zhangxin5@kingsoft.com>
2015-07-20 13:35:20 +0800

    basic calculator

---
+++ basic-calculator/basic_calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+#include <ctype.h>
+
+using namespace std;
+
+char* readNext(const char *ptr)
+{
+	return null;
+}
+
+char* mid2post(const char* ptr)
+{
+	char* rtnBuf = (char*)malloc(strlen(str) * sizeof(char));
+	stack<char> op_statck;
+	char* entry = readNext(ptr);
+	if(!isdigit(entry[0])) {
+
+	} else {
+		
+	}
+
+}
+
+int main()
+{
+
+}
