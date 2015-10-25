099d587da0a28d8a68c7f089bc593d3b24237196
zhangxin <xin_zhang23@126.com>
2015-10-25 20:51:45 +0800

    add two new files

---
+++ linear-list/array/set_matrix_zeroes.cpp
@@ -0,0 +1,58 @@
+/**
+ *Given a m × n matrix, if an element is 0, set its entire row and column to 0.
+ *Do it in place.
+ *Follow up: Did you use extra space?
+ *A straight forward solution using O(mn) space is probably a bad idea.
+ *A simple improvement uses O(m + n) space, but still not the best solution.
+ *Could you devise a constant space solution?
+ * */
+
+class Solution {
+    public:
+        void setZeroes(vector<vector<int> >& matrix) {
+            const size_t m = matrix.size();
+            const size_t n = matrix[0].size();
+            bool row_has_zero = false;
+            bool col_has_zero = false;
+
+            for(size_t i = 0; i < n; i++) {
+                if(matrix[0][i] == 0) {
+                    row_has_zero = true;
+                    break;
+                }
+            }
+
+            for(size_t i = 0; i < m; i++) {
+                if(matrix[i][0] == 0) {
+                    col_has_zero = true;
+                    break;
+                }
+            }
+
+            for(size_t i = 1; i < m; i++) {
+                for(size_t j = 1; j < n; j++) {
+                    if(matrix[i][j] == 0) {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+
+            for(size_t i = 1; i < m; i++) {
+                for(size_t j = 1; j < n; j++) {
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                        matrix[i][j] == 0;
+                }
+            }
+
+            if(row_has_zero) {
+                for(size_t i = 0; i < n; i++)
+                    matrix[0][i] = 0;
+            }
+
+            if(col_has_zero) {
+                for(size_t i = 0; i < n; i++)
+                    matrix[i][0] = 0;
+            }
+        }
+};
