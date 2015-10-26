3ea89d8962da715da07d84c2e928fc2998b7d687
zhangxin5 <zhangxin5@kingsoft.com>
2015-10-26 09:29:26 +0800

    candy

---
+++ linear-list/array/candy.cpp
@@ -0,0 +1,39 @@
+/**
+ *There are N children standing in a line. Each child is assigned a rating value.
+ *You are giving candies to these children subjected to the following requirements:
+ *  • Each child must have at least one candy.
+ *  • Children with a higher rating get more candies than their neighbors.
+ *What is the minimum candies you must give?
+ * */
+
+class Solution {
+    public:
+        int candy(vector<int>& ratings) {
+            const int n = ratings.size();
+            vector<int> increments(n);
+
+            int inr = 1;
+            for(int i = 0; i < n; i++) {
+                if(ratings[i] > ratings[i - 1]) {
+                    increments[i] = max(increments[i], inr++);
+                } else {
+                    inr = 1;
+                }
+            }
+
+            inr = 1;
+            for(int i = n - 2; i >= 0; i--) {
+                if(ratings[i] > ratings[i + 1]) {
+                    increments[i] = max(increments[i + 1], inr++);
+                } else {
+                    inr = 1;
+                }
+            }
+
+            for(int i = 0; i < n; i++) {
+                increments[i]++;
+            }
+
+            return increments;
+        }
+};
