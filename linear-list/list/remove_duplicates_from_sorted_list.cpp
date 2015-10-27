53b8cbfe6942dc2fdb9464076b23701e9e5ab2d8
zhangxin5 <zhangxin5@kingsoft.com>
2015-10-27 09:37:39 +0800

    remove duplicates from sorted list

---
+++ linear-list/list/remove_duplicates_from_sorted_list.cpp
@@ -0,0 +1,26 @@
+/**
+ *Given a sorted linked list, delete all duplicates such that each element appear only once.
+ *For example,
+ *Given 1->1->2, return 1->2.
+ *Given 1->1->2->3->3, return 1->2->3.
+ * */
+
+class Solution {
+    public:
+        ListNode* removeDulicates(ListNode* head) {
+            if(head == null)
+                return null;
+
+            for(ListNode *prev = head, *cur = head->next; cur; cur = cur->next) {
+                if(prev->val == cur->val) {
+                    prev->next = cur->next;
+                    delete cur;
+                    cur = prev;
+                } else {
+                    prev = cur;
+                }
+            }
+
+            return head;
+        }
+};
