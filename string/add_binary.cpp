666a070225ae3cfcef7abd2bf2f92451cafbc471
zhangxin <xin_zhang23@126.com>
2015-10-31 11:19:13 +0800

    add binary

---
+++ string/add_binary.cpp
@@ -0,0 +1,30 @@
+/**
+ *Given two binary strings, return their sum (also a binary string).
+ *For example,
+ *  a = "11"
+ *  b = "1"
+ *  return "100"
+ * */
+
+class Solution {
+    public:
+        string addBinary(string a, string b) {
+            string result;
+            const size_t n = a.size() > b.size() ? a.size() : b.size();
+            reverse(a.begin(), a.end());
+            reverse(b.begin(), b.end());
+            int carry = 0;
+            for(size_t i = 0; i < n; i++) {
+                const int ai = i < a.size() ? a[i] - '0' : 0;
+                const int bi = i < b.size() ? b[i] - '0' : 0;
+                const int val = (ai + bi + carry) % 2;
+                carry = (ai + bi + carry) / 2;
+                result.insert(result.begin(), val + '0');
+            }
+
+            if(carry == 1)
+                result.insert(result.begin(), '1');
+
+            return result;
+        }
+};
