5b284b42620cd32489f6332f084c3434b7e1d042
zhangxin5 <zhangxin5@kingsoft.com>
2015-11-02 09:27:04 +0800

    wildcard match

---
+++ string/wildcard_matching.cpp
@@ -0,0 +1,40 @@
+/**
+ *Implement wildcard pattern matching with support for '?' and '*'.
+ *'?' Matches any single character. '*' Matches any sequence of
+ *characters (including the empty sequence).
+ *The matching should cover the entire input string (not partial).
+ *The function prototype should be:
+ *  bool isMatch(const char *s, const char *p)
+ *Some examples:
+ *  isMatch("aa","a") → false
+ *  isMatch("aa","aa") → true
+ *  isMatch("aaa","aa") → false
+ *  isMatch("aa", "*") → true
+ *  isMatch("aa", "a*") → true
+ *  isMatch("ab", "?*") → true
+ *  isMatch("aab", "c*a*b") → false
+ * */
+
+class Solution {
+    public:
+        bool isMatch(const char* s, const char* p) {
+            if(*p == '*') {
+                while(*p == '*')
+                    p++;
+
+                if(*p == '\0')
+                    return true;
+
+                while(*s && !isMatch(s, p))
+                    s++;
+
+                return *s != '\0';
+            } else if(*p == '\0' || *s == '\0') {
+                return *p == *s;
+            } else if(*p == *s || *p == '?') {
+                return isMatch(s++, p++);
+            } else {
+                return false;
+            }
+        }
+};
