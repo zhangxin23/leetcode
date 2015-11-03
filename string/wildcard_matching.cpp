a3eef9430de8a177ea2c6dc0184734ddde7baae4
zhangxin <xin_zhang23@126.com>
2015-11-03 20:57:57 +0800

    wildcard matching iteration

--- string/wildcard_matching.cpp
+++ string/wildcard_matching.cpp
@@ -38,4 +38,37 @@ class Solution {
                 return false;
             }
         }
+
+        bool isMatch_iteration(const char *s, const char *p) {
+            bool star = false;
+            const char *str, *ptr;
+            for(str = s, ptr = p; *str; str++, ptr++) {
+                switch(*ptr) {
+                    case '?':
+                        break;
+                    case '*':
+                        star = true;
+                        s = str, p = ptr;
+                        while(*p == '*')
+                            p++;
+                        if(*p == '\0')
+                            return true;
+                        str = s - 1;
+                        ptr = p - 1;
+                        break;
+                    default:
+                        if(*str != *ptr) {
+                            if(!star)
+                                return false;
+                            s++;
+                            str = s - 1;
+                            ptr = p - 1;
+                        }
+                }
+            }
+            while(*ptr == '*')
+                ptr++;
+
+            return  *ptr == '\0';
+        }
 };
