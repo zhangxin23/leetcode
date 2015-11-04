ef44bbaf66865b90f0b9fbd4e516347aa9ac893d
zhangxin <xin_zhang23@126.com>
2015-11-04 21:30:59 +0800

    valid parentheses

---
+++ stack/valid_parentheses.cpp
@@ -0,0 +1,27 @@
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * The brackets must close in the correct order, "()" and "()[]" are all valid
+ * but "(]" and "([)]" are not.
+ * */
+
+class Solution {
+    public:
+        bool isValid(const string &s) {
+            string left = "([{";
+            string right = ")]}";
+            stack<char> stk;
+
+            for(auto c: s) {
+                if(left.find(c) != string::npos) {
+                    stk.push(c);
+                } else {
+                    if(stk.empty() || stk.top() != left[right.find(c)])
+                        return false;
+                    else
+                        stk.pop();
+                }
+            }
+            return stk.empty();
+        }
+};
