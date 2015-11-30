a5b71f068461d6559b743e48442773a9027f608a
zhangxin5 <zhangxin5@kingsoft.com>
2015-11-30 09:59:48 +0800

    max area

---
+++ greedy_algorithm/container_with_most_water.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ *
+ * Note: You may not slant the container.
+ * */
+
+class Solution {
+public:
+    int maxArea(const vector<int> &heights) {
+        int start = 0, end = heights.size() - 1;
+        int result = 0;
+        while(start <= end) {
+            int area = min(heights[start], heights[end]) * (end - start);
+            result = max(result, area);
+
+            if(heights[start] <= heights[end])
+                start++;
+            else
+                end--;
+        }
+
+        return result;
+    }
+};
