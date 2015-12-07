811d86dbe12cb0df89c5d79d2d796e48d2ea09ac
zhangxin5 <zhangxin5@kingsoft.com>
2015-12-07 09:36:36 +0800

    minimum window substring

---
+++ detail/minimum_window_substring.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+ *
+ * For example,
+ * S = "ADOBECODEBANC"
+ * T = "ABC"
+ * Minimum window is "BANC".
+ *
+ * Note:
+ * If there is no such window in S that covers all characters in T, return the empty string "".
+ *
+ * If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
+ * */
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int appeared[256] = {0};
+        int expected[256] = {0};
+
+        for(int i = 0; i < t.size(); i++) {
+            expected[t[i]]++;
+        }
+
+        int start = 0;
+        int appeare_count = 0;
+        int minStart = 0;
+        int minWidth = INT32_MAX;
+        for(int end = 0; end < s.size(); end++) {
+            if(expected[s[end]] > 0) {
+                appeared[s[end]]++;
+                if(appeared[s[end]] >= expected[s[end]]) {
+                    appeare_count++;
+                }
+            }
+
+            if(appeare_count >= t.size()) {
+                while((appeared[s[start]] > expected[s[start]]) || expected[s[start]] == 0) {
+                    appeared[s[start]]--;
+                    start++;
+                }
+
+                if(minWidth > (end - start - 1)) {
+                    minWidth = end - start - 1;
+                    minStart = start;
+                }
+            }
+        }
+
+        if(minWidth == INT32_MAX)
+            return "";
+        else
+            return s.substr(minStart, minWidth);
+    }
+};
