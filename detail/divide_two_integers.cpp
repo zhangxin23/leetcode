b60fea5a4888b0391532d7f1f445c08b69005cc1
zhangxin <xin_zhang23@126.com>
2015-12-08 22:22:10 +0800

    divide two integers, zigzag conversion

---
+++ detail/divide_two_integers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Divide two integers without using multiplication, division and mod operator.
+ *
+ * If it is overflow, return MAX_INT.
+ * */
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long long a = dividend >= 0 ? dividend : -(long long)dividend;
+        long long b = divisor >= 0 ? divisor : -(long long)divisor;
+
+        long long result = 0;
+        while(a >= b) {
+            long long c = b;
+            for(int i = 0; a >= c; ++i, c <<= 1) {
+                a -= c;
+                result += 1 << i;
+            }
+        }
+
+        return ((dividend ^ divisor) >> 31) ? (-result) : (result);
+    }
+};
