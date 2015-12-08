6ae9cc0b93e9b757140236326f74a2e265e439c8
zhangxin5 <zhangxin5@kingsoft.com>
2015-12-08 10:06:45 +0800

    spiral matrix

---
+++ detail/spiral_matrix.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ *
+ * For example,
+ * Given the following matrix:
+ *
+ *   [
+ *    [ 1, 2, 3 ],
+ *    [ 4, 5, 6 ],
+ *    [ 7, 8, 9 ]
+ *   ]
+ * You should return [1,2,3,6,9,8,7,4,5].
+ * */
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int> >& matrix) {
+        vector<int> result;
+        if(matrix == NULL || matrix.size() <= 0 || matrix[0].size() <= 0)
+            return result;
+
+        int x_begin = 0, x_end = matrix[0].size() - 1;
+        int y_begin = 0, y_end = matrix.size() - 1;
+
+        while(true) {
+            for(int i = x_begin; i <= x_end; i++)
+                result.push_back(matrix[y_begin][i]);
+            if(y_begin++ > y_end)
+                break;
+
+            for(int j = y_begin; j <= y_end; j++)
+                result.push_back(matrix[j][x_end]);
+            if(x_end-- < x_begin)
+                break;
+
+            for(int i = x_end; i >= x_begin; i--)
+                result.push_back(matrix[y_end][i]);
+            if(y_end-- < y_begin)
+                break;
+
+            for(int j = y_end; j >= y_begin; j--)
+                result.push_back(matrix[j][x_begin]);
+            if(x_begin++ > x_end)
+                break;
+        }
+
+        return result;
+    }
+};
---
+++ detail/spiral_matrix_II.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+ *
+ * For example,
+ * Given n = 3,
+ *
+ * You should return the following matrix:
+ *   [
+ *    [ 1, 2, 3 ],
+ *    [ 8, 9, 4 ],
+ *    [ 7, 6, 5 ]
+ *   ]
+ * */
+
+class Solution {
+public:
+    vector<vector<int> > generateMatrix(int n) {
+        if(n == 0)
+            return NULL;
+
+        vector<vector<int> > result(n, vector<int>(n, 0));
+
+        int x_begin = 0, x_end = n - 1;
+        int y_begin = 0, y_end = n - 1;
+        int num = 1;
+
+        while(true) {
+            for(int i = x_begin; i <= x_end; i++)
+                result[y_begin][i] = num++;
+            if(y_begin++ > y_end)
+                break;
+
+            for(int j = y_begin; j <= y_end; j++)
+                result[j][x_end] = num++;
+            if(x_end-- < x_begin)
+                break;
+
+            for(int i = x_end; i >= x_begin; i--)
+                result[y_end][i] = num++;
+            if(y_end-- < y_begin)
+                break;
+
+            for(int j = y_end; j >= y_begin; j--)
+                result[j][x_begin] = num++;
+            if(x_begin++ > x_end)
+                break;
+        }
+
+        return result;
+    }
+};
