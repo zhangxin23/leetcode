13920c4b958b788f1fb7c71d200ac8982c47d1ff
zhangxin <xin_zhang23@126.com>
2016-01-11 22:15:10 +0800

    fix bugs

--- dynamic_programming/triangle.cpp
+++ dynamic_programming/triangle.cpp
@@ -15,6 +15,9 @@ using namespace std;
  *     [4,1,8,3]
  *   ]
  * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ *
+ * 状态方程: f[j][j]表示从(i, j)出发,路径的最小和.
+ *      f[i][j] = min(f[i + 1][j], f[i + 1][j + 1]) + f[i][j];
  * */
 
 class Solution {
@@ -22,7 +25,7 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         for(int i = triangle.size() - 2; i != 0; i--) {
             for(int j = 0; j < i + 1; j++) {
-                triangle[i][j] = min(triangle[i][j + 1], triangle[i + 1][j + 1]) + triangle[i][j];
+                triangle[i][j] = min(triangle[i + 1][j], triangle[i + 1][j + 1]) + triangle[i][j];
             }
         }
 
