419520f76310d865e182ea744eb75ddeba392567
zhangxin5 <zhangxin5@kingsoft.com>
2016-01-18 18:20:19 +0800

    gray code: edit comment

--- linear-list/array/gray_code.cpp
+++ linear-list/array/gray_code.cpp
@@ -13,8 +13,8 @@ using namespace std;
  *  11 - 3
  *  10 - 2
  *Note:
- *  • Foragivenn,agraycodesequenceisnotuniquelydefined.
- *  • Forexample,isalsoavalidgraycodesequenceaccordingtotheabovedefinition.
+ *  • For a given n, a gray code sequence is not uniquely defined.
+ *  • For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
  *  • For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
  * */
 
