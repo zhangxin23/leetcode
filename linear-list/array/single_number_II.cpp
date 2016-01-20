91e430078fdcdec51381dad4cf96483f8c8016bc
zhangxin <xin_zhang23@126.com>
2016-01-20 22:15:09 +0800

    fix bugs

--- linear-list/array/single_number_II.cpp
+++ linear-list/array/single_number_II.cpp
@@ -1,14 +1,15 @@
 /**
- *Given an array of integers, every element appears three times except for one.
- *Find that single one.
- *Note: Your algorithm should have a linear runtime complexity.
- *Could you implement it without using extra memory?
+ * Given an array of integers, every element appears three times except for one.
+ * Find that single one.
+ * Note: Your algorithm should have a linear runtime complexity.
+ * Could you implement it without using extra memory?
  * */
 
 class Solution {
-    public singleNumber(int A[], n) {
+public:
+    int singleNumber(int A[], int n) {
         int w = sizeof(int) * 8;
-        int count[w];
+        int *count = new int[w];
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < w; j++) {
