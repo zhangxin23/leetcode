b791708f8175b3ec29a3ccaf8781aa28c126ebc1
zhangxin5 <zhangxin5@kingsoft.com>
2016-01-25 11:49:07 +0800

    fix bugs

--- string/regular_expression_matching.cpp
+++ string/regular_expression_matching.cpp
@@ -1,6 +1,7 @@
 /**
  * Implement regular expression matching with support for '.' and '*'.
- * '.' Matches any single character. '*' Matches zero or more of the preceding element.
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
  * The matching should cover the entire input string (not partial).
  *
  * The function prototype should be:
