1edb2e46f8b84421c52f60f41a62d4ea6a83d161
zhangxin5 <zhangxin5@kingsoft.com>
2016-02-02 16:29:55 +0800

    fix bugs

--- linear-list/array/next_permutation.cpp
+++ linear-list/array/next_permutation.cpp
@@ -5,97 +5,97 @@
 using namespace std;
 
 /**
- *Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
- *If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
- *The replacement must be in-place, do not allocate extra memory.
- *Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
- * 1, 2, 3 -> 1, 3, 2
- * 3, 2, 1 -> 1, 2, 3
- * 1, 1, 5 -> 1, 5, 1
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * The replacement must be in-place, do not allocate extra memory.
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ *  1, 2, 3 -> 1, 3, 2
+ *  3, 2, 1 -> 1, 2, 3
+ *  1, 1, 5 -> 1, 5, 1
  * */
 
 class Solution {
-    public:
-        void next_permutation(vector<int> &nums) {
-            int n = nums.size();
-            if(n < 2)
-                return;
+public:
+    void next_permutation(vector<int> &nums) {
+        int n = nums.size();
+        if(n < 2)
+            return;
 
-            int pivot = -1;
-            for(int i = n - 1; i >= 1; i--) {
-                if(nums[i - 1] < nums[i]) {
-                    pivot = i - 1;
-                    break;
-                }
-            }
-
-            if(pivot == -1) {
-                reverse(nums.begin(), nums.end());
-                return;
-            }
-
-            for(int i = n - 1; i > pivot; i--) {
-                if(nums[i] > nums[pivot]) {
-                    swap(nums[i], nums[pivot]);
-                    break;
-                }
-            }
-
-            reverse(next(nums.begin(), pivot + 1), nums.end());
-        }
-
-        bool nextPermutation(int A[], int n) {
-            if(n == 0 || n == 1)
-                return false;
-
-            //find pivot number
-            int pivot = -1;
-            for(int i = n - 1; i > 0; i--) {
-                if(A[i] > A[i - 1])
-                    pivot = i - 1;
-                    break;
-            }
-
-            if(pivot == -1) {
-                reverse(A, n, 0, n - 1);
-                return false;
-            }
-
-            //find change number
-            for(int i = n - 1; i >= pivot; i--) {
-                if(A[i] > A[pivot]) {
-                    //swap pivot number and change number
-                    swap_self(A + pivot, A + i);
-                    break;
-                }
-            }
-
-            //reverse pivot to last
-            reverse(A, n, pivot + 1, n - 1);
-            return true;
-        }
-
-    private:
-        void swap_self(int* a, int* b) {
-            *a ^= *b;
-            *b = *a ^ *b;
-            *a = *a ^ *b;
-        }
-
-        void reverse(int A[], int n, int start, int end) {
-            if(start < 0)
-                return;
-
-            if(end > n)
-                return;
-
-            if(n == 0 || n == 1)
-                return;
-
-            while(start <= end) {
-                swap(A + start, A + end);
-                start++;
-                end--;
+        int pivot = -1;
+        for(int i = n - 1; i >= 1; i--) {
+            if(nums[i - 1] < nums[i]) {
+                pivot = i - 1;
+                break;
             }
         }
+
+        if(pivot == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i = n - 1; i > pivot; i--) {
+            if(nums[i] > nums[pivot]) {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        reverse(next(nums.begin(), pivot + 1), nums.end());
+    }
+
+    bool nextPermutation(int A[], int n) {
+        if(n == 0 || n == 1)
+            return false;
+
+        //find pivot number
+        int pivot = -1;
+        for(int i = n - 1; i > 0; i--) {
+            if(A[i] > A[i - 1])
+                pivot = i - 1;
+                break;
+        }
+
+        if(pivot == -1) {
+            reverse_self(A, n, 0, n - 1);
+            return false;
+        }
+
+        //find change number
+        for(int i = n - 1; i >= pivot; i--) {
+            if(A[i] > A[pivot]) {
+                //swap pivot number and change number
+                swap_self(A + pivot, A + i);
+                break;
+            }
+        }
+
+        //reverse pivot to last
+        reverse_self(A, n, pivot + 1, n - 1);
+        return true;
+    }
+
+private:
+    void swap_self(int* a, int* b) {
+        *a ^= *b;
+        *b = *a ^ *b;
+        *a = *a ^ *b;
+    }
+
+    void reverse_self(int A[], int n, int start, int end) {
+        if(start < 0)
+            return;
+
+        if(end > n)
+            return;
+
+        if(n == 0 || n == 1)
+            return;
+
+        while(start <= end) {
+            swap_self(A + start, A + end);
+            start++;
+            end--;
+        }
+    }
 };
--- linear-list/array/permulation_sequence.cpp
+++ linear-list/array/permulation_sequence.cpp
@@ -5,60 +5,83 @@
 using namespace std;
 
 /**
- *The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
- *By listing and labeling all of the permutations in order, We get the following sequence (ie, for n = 3):
+ * The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+ * By listing and labeling all of the permutations in order, We get the following sequence (ie, for n = 3):
  *  123
  *  132
  *  213
  *  231
  *  312
  *  321
- *Given n and k, return the kth permutation sequence.
- *Note: Given n will be between 1 and 9 inclusive.
+ * Given n and k, return the kth permutation sequence.
+ * Note: Given n will be between 1 and 9 inclusive.
  * */
 
 class Solution {
-    public:
-        vector<int> kthPermutation(int n, int k) {
-            vector<int> result;
+public:
+    vector<int> kthPermutation(int n, int k) {
+        vector<int> result;
 
-            for(int i = 0; i < n; i++) {
-                result.push_back(i + 1);
-            }
-
-            for(int i = 0; i < k - 1; i++) {
-                nextPermutation(result);
-            }
-
-            return result;
+        for(int i = 0; i < n; i++) {
+            result.push_back(i + 1);
         }
 
-    private:
-        void nextPermutation(vector<int> &vec) {
-            int n = vec.size();
-            if(n < 2)
-                return;
-
-            int pivot = -1;
-            for(int i = n - 1; i >= 1; i--) {
-                if(vec[i - 1] < vec[i]) {
-                    pivot = i - 1;
-                    break;
-                }
-            }
-
-            if(pivot == -1) {
-                reverse(vec.begin(), vec.end());
-                return;
-            }
-
-            for(int i = n - 1; i > pivot; i--) {
-                if(vec[i] > vec[pivot]) {
-                    swap(vec[i], vec[pivot]);
-                    break;
-                }
-            }
-
-            reverse(next(vec.begin(), pivot + 1), vec.end());
+        for(int i = 0; i < k - 1; i++) {
+            nextPermutation(result);
         }
+
+        return result;
+    }
+
+    string getPermutation(int n, int k) {
+        string result;
+        vector<int> factorial(n, 1);
+        vector<char> nums(n, 1);
+
+        for(int i = 1; i < n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = (i + 1) + '0';
+        }
+
+        k--;
+        for(int i = n; i >= 1; i--) {
+            int j = k / factorial[i - 1];
+            result.push_back(nums[j]);
+            k = k % factorial[i - 1];
+            nums.erase(nums.begin() + j);
+        }
+        return result;
+    }
+
+private:
+    void nextPermutation(vector<int> &vec) {
+        int n = vec.size();
+        if(n < 2)
+            return;
+
+        int pivot = -1;
+        for(int i = n - 1; i >= 1; i--) {
+            if(vec[i - 1] < vec[i]) {
+                pivot = i - 1;
+                break;
+            }
+        }
+
+        if(pivot == -1) {
+            reverse(vec.begin(), vec.end());
+            return;
+        }
+
+        for(int i = n - 1; i > pivot; i--) {
+            if(vec[i] > vec[pivot]) {
+                swap(vec[i], vec[pivot]);
+                break;
+            }
+        }
+
+        reverse(next(vec.begin(), pivot + 1), vec.end());
+    }
 };
