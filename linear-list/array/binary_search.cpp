4b269ab66a9853ad36c2c8bdbbee353203e628cb
zhangxin5 <zhangxin5@kingsoft.com>
2016-02-15 09:25:51 +0800

    edit formatter

--- linear-list/array/binary_search.cpp
+++ linear-list/array/binary_search.cpp
@@ -1,22 +1,22 @@
 /**
- *Binary Search
+ * Binary Search
  * */
 
 class Solution {
-    public:
-        int binarySearch(int A[], int n, int target) {
-            int first = 0, last = n;
-            while(first != last) {
-                int mid = first + (last - first) / 2; //prevente overflow
+public:
+    int binarySearch(int A[], int n, int target) {
+        int first = 0, last = n;
+        while(first != last) {
+            int mid = first + (last - first) / 2; //prevente overflow
 
-                if(A[mid] == target) {
-                    return mid;
-                } else if(A[mid] < target) {
-                    first = mid + 1;
-                } else {
-                    last = mid;
-                }
+            if(A[mid] == target) {
+                return mid;
+            } else if(A[mid] < target) {
+                first = mid + 1;
+            } else {
+                last = mid;
             }
-            return -1;
         }
+        return -1;
+    }
 };
